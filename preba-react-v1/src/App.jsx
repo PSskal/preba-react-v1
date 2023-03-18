7428da64c66c0a3f53ba47cd8429b7840f46d953
Richard Pascual <ricpascual@gmail.com>
2023-03-18 08:56:23 -0500

    purba finalizada

--- preba-react-v1/src/App.jsx
+++ preba-react-v1/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+const URLApi= "https://catfact.ninja/fact"
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState();
 
+  const getData = async () => {
+    const data = await fetch(URLApi);
+    const response = await data.json();
+    setFact(response.fact);
+    const firstWord = response.fact.split(' ')[0];
+    console.log(firstWord);
+    
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>app de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {/* <p>{firstWord}</p> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
